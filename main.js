e7a2c44129e1ead237c50741d79eb55214b657ac
Raul Rafael Balane <raulbalane7@gmail.com>
2022-06-28 17:11:15 +0800

    Added few functionalities and styling

--- main.js
+++ main.js
@@ -3,20 +3,113 @@ const pad = document.querySelector("#sketch-pad-cont")
 const gridS = document.querySelector("#grid-sides")
 const gridI = document.querySelector("#grid-info")
 const clear = document.querySelector("#clear")
+const reset = document.querySelector("#reset")
+const eraser = document.querySelector("#eraser")
+const buttons = document.querySelectorAll(".button")
+const color = document.querySelector("#color-mode")
+const cl = document.querySelector("#color-label")
+
+const Default_Color = "#000"
+const Eraser_Color = "#fff"
+const Default_Sides = 64
+let Current_Color = Default_Color;
+let ColorMode_Color;
+let RandomMode_Color;
+
+function setCurrentColor(newColor){
+    Current_Color = newColor;
+}
+
+
+gridS.onmousemove = (e) => updateGridInfo(e.target.value)
+gridS.onchange = (e) => setGridBoxSides(e.target.value)
+color.onchange = (e) => setCurrentColor(e.target.value)
+
+function updateGridInfo(value) {
+    gridI.textContent = `Grid Box Sides: ${value} X ${value}`
+}
+
+function setGridBoxSides(value) {
+    resetPad()
+    setUpGrid(value);
+}
+
+
+clear.addEventListener("click", clearPad)
+reset.addEventListener("click", resetPad)
+eraser.addEventListener("click", eraserActivate)
+// color.addEventListener("click",colorMode)
+
+// function colorMode(){
+//     setUpGrid(setGridBoxSides.value,Current_Color)    
+// }
+
+function eraserActivate(){
+    Current_Color = Eraser_Color;
+    divBox.addEventListener("mouseover", fillBox);
+    divBox.addEventListener("click", () => divBox.style.backgroundColor = Eraser_Color);
+    divBox.addEventListener("mousedown", () => divBox.style.backgroundColor = Eraser_Color);
+
+}
+
+function resetPad(){
+    pad.innerHTML= "";
+    setUpGrid(Default_Sides);
+    Current_Color = Default_Color;
+    updateGridInfo(Default_Sides)
+    gridS.setAttribute("value",`${Default_Sides}`)
+    
+}
+
+
+function clearPad(){
+    const gridBoxes = document.querySelectorAll(".grid-box")
+    gridBoxes.forEach((grid) => {
+        grid.style.backgroundColor = "";
+    })    
+}
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () =>{
+        buttons.forEach((button) => {
+            if (button.classList.contains("button-selected") || 
+            cl.classList.contains("color-selected")){
+                button.classList.remove("button-selected")
+                cl.classList.remove("color-selected")
+            }
+        });
+        if (button.classList.contains("button-selected")){
+            button.classList.remove("button-selected")
+        } else button.classList.add("button-selected");
+        if (color.classList.contains("button-selected")){
+            cl.classList.add("color-selected")
+        }    
+    })
+})
+
+
 
 
 // Creating the pad boxes grid in the main container
-for(let x =0; x<4096;){
-    const divBox = document.createElement("div");
-    divBox.classList.add("grid-box");
-    divBox.addEventListener("mouseover", fillBox);
-    divBox.addEventListener("click", () => divBox.style.backgroundColor = "black");
-    divBox.addEventListener("mousedown", () => divBox.style.backgroundColor = "black");
-    divBox.setAttribute("draggable", "false");
-    pad.appendChild(divBox);
-    x++;
+function setUpGrid(sides = Default_Sides){
+    pad.style.gridTemplateColumns = `repeat(${sides},1fr)`
+    pad.style.gridTemplateRows = `repeat(${sides},1fr)` 
+
+    for(let x =0; x < sides * sides;){
+        const divBox = document.createElement("div");
+        divBox.classList.add("grid-box");
+        divBox.addEventListener("mouseover", fillBox);
+        divBox.addEventListener("click", () => divBox.style.backgroundColor = Current_Color);
+        divBox.addEventListener("mousedown", () => divBox.style.backgroundColor = Current_Color);
+        divBox.setAttribute("draggable", "false");
+        pad.appendChild(divBox);
+        x++;
     
+    }
+
 }
+
+
 
 let isMouseDown = false;
 document.body.onmousedown = () => (isMouseDown = true);
@@ -26,23 +119,10 @@ document.body.setAttribute("ondrop", "return false");
 
 function fillBox(e) {
     if (e.type === "mouseover" && !isMouseDown) return undefined;
-    else {e.target.style.backgroundColor="black" }
+    else {e.target.style.backgroundColor= Current_Color}
 }
 
 
-
-gridS.onmousemove = (e) => updateGridInfo(e.target.value)
-
-function updateGridInfo(value) {
-    gridI.textContent = `Grid Box Sides: ${value} X ${value}`
-}
-
-
-clear.addEventListener("click", resetPad)
-
-function resetPad(){
-    const gridBoxes = document.querySelectorAll(".grid-box")
-    gridBoxes.forEach((grid) => {
-        grid.style.backgroundColor = "white";       
-    })
+window.onload = () =>{
+    setUpGrid(Default_Sides)
 }
